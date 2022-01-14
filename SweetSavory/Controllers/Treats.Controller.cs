0447582cbcb44968b26646055e65fff89ca58f23
Kaxon <karenaxon@users.noreply.github.com>
2022-01-14 14:55:30 -0800

    Add treat controller to project.

---
+++ SweetSavory/Controllers/Treats.Controller.cs
@@ -0,0 +1,36 @@
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SweetSavory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace SweetSavory.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly SweetSavoryContext _db;
+    private readonly UserManager<ApplicationUser>_userManager;
+
+    public TreatsController(UserManager<ApplicationUser> userManager, SweetSavoryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userTreats);
+    }
+
+
+  }
+}
