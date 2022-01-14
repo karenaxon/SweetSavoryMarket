f7eab0428d41c7a903a63461b5e32adffe27f792
Kaxon <karenaxon@users.noreply.github.com>
2022-01-14 15:15:17 -0800

    Modified to show list of treats without requiring a login.

--- SweetSavory/Controllers/Treats.Controller.cs
+++ SweetSavory/Controllers/Treats.Controller.cs
@@ -23,13 +23,19 @@ namespace SweetSavory.Controllers
       _db = db;
     }
 
-    public async Task<ActionResult> Index()
+    public ActionResult Index()
     {
-      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-      var currentUser = await _userManager.FindByIdAsync(userId);
-      var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
-      return View(userTreats);
+      List<Treat> model = _db.Treats.ToList();
+      return View(model);
     }
+
+    // public async Task<ActionResult> Index()
+    // {
+    //   var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    //   var currentUser = await _userManager.FindByIdAsync(userId);
+    //   var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
+    //   return View(userTreats);
+    // }
 
 
   }
