f1495a7e3325765c42b0f8dd6059bf95593e2e29
Kaxon <karenaxon@users.noreply.github.com>
2022-01-14 11:16:59 -0800

    Add dabase set up files including sweetsavory join.

---
+++ SweetSavory/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,28 @@
+using System.Collections.Immutable;
+using System.IO;
+using System.Security.AccessControl;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SweetSavory.Models
+{
+  public class SweetSavoryContextFactory: IDesignTimeDbContextFactory<SweetSavoryContext>
+  {
+    SweetSavoryContext IDesignTimeDbContextFactory<SweetSavoryContext>.CreateDbContext(string[] args)
+    {
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json")
+          .Build();
+      
+      var builder = new DbContextOptionsBuilder<SweetSavoryContext>();
+
+      builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"],
+      ServerVersion.AutoDetect(configuration ["ConnectionStrings:DefaultConnection"]));
+
+      return new SweetSavoryContext(builder.Options);
+    }
+  }
+}
