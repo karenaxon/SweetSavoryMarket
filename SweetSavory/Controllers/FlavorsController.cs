7e72054b4d0ce1660afdd162749be800237d17d7
Kaxon <karenaxon@users.noreply.github.com>
2022-01-14 16:51:47 -0800

    Add user to flavor class.

--- SweetSavory/Controllers/FlavorsController.cs
+++ SweetSavory/Controllers/FlavorsController.cs
@@ -1,17 +1,25 @@
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using SweetSavory.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace SweetSavory.Controllers
 {
   public class FlavorsController : Controller
   {
     private readonly SweetSavoryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public FlavorsController(SweetSavoryContext db)
+    public FlavorsController(UserManager<ApplicationUser> userManager,SweetSavoryContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
 
@@ -19,6 +27,14 @@ namespace SweetSavory.Controllers
     {
       List<Flavor> model = _db.Flavors.ToList();
       return View(model);
+    }
+
+    public async Task<ActionResult> Create()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userFlavors = _db.Flavors.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userFlavors);
     }
 
   }
