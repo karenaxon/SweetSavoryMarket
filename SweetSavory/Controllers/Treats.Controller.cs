20e41663c01340f318f80f224d282154ef7e2465
Kaxon <karenaxon@users.noreply.github.com>
2022-01-17 10:47:39 -0800

    Add create treats.

--- SweetSavory/Controllers/Treats.Controller.cs
+++ SweetSavory/Controllers/Treats.Controller.cs
@@ -29,14 +29,28 @@ namespace SweetSavory.Controllers
       return View(model);
     }
 
-    // public async Task<ActionResult> Index()
-    // {
-    //   var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-    //   var currentUser = await _userManager.FindByIdAsync(userId);
-    //   var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
-    //   return View(userTreats);
-    // }
+    [Authorize]
+    public ActionResult Create()
+    {
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View();
+    }
 
+    [HttpPost]
+    public async Task<ActionResult> Create(Treat treat, int FlavorId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
   }
 }
