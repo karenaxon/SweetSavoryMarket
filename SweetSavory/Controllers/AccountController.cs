1ea1a5c9105e651e38d27e4554b9918178cd1ad6
Kaxon <karenaxon@users.noreply.github.com>
2022-01-17 11:00:52 -0800

    Add account login page.

--- SweetSavory/Controllers/AccountController.cs
+++ SweetSavory/Controllers/AccountController.cs
@@ -50,6 +50,20 @@ namespace SweetSavory.Controllers
     {
       return View();
     }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View("Register");
+      }
+    }
     
   }
 }
