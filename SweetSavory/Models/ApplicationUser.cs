7169867db418812b934677e2d3c127a7398a3d62
Kaxon <karenaxon@users.noreply.github.com>
2022-01-23 17:18:36 -0800

    Comment out properties for later use.

--- SweetSavory/Models/ApplicationUser.cs
+++ SweetSavory/Models/ApplicationUser.cs
@@ -1,9 +1,34 @@
+using System.IO;
+using System;
+using System.Security.AccessControl;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
 namespace SweetSavory.Models
 {
   public class ApplicationUser : IdentityUser
   {
+    // [Required]
+    // [Display(Name = "First Name")]
+    // public string FirstName { get; set; }
+
+    // [Required]
+    // [Display(Name = "Last Name")]
+    // public string LastName { get; set; }
+
+    // [Required]
+    // [Display(Name = "User Name")]
+    // public string UserName { get; set; }
     
+    // [Required]
+    // [EmailAddress]
+    // [Display(Name = "Email")]
+    // public string Email { get; set; }
+
+    // [Required]
+    // [DataType(DataType.Password)]
+    // [Display(Name = "Password")]
+    // public string Password { get; set; }
   }
 }
