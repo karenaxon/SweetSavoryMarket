abafa2d32f2df64d915fa49ca08be0099bd5729b
Kaxon <karenaxon@users.noreply.github.com>
2022-01-23 16:52:22 -0800

    Update styles.

--- SweetSavory/ViewModels/RegisterViewModel.cs
+++ SweetSavory/ViewModels/RegisterViewModel.cs
@@ -4,13 +4,13 @@ namespace SweetSavory.ViewModels
 {
   public class RegisterViewModel
   {
-    [Required]
-    [Display(Name = "First Name")]
-    public string FirstName { get; set; }
+    // [Required]
+    // [Display(Name = "First Name")]
+    // public string FirstName { get; set; }
 
-    [Required]
-    [Display(Name = "Last Name")]
-    public string LastName { get; set; }
+    // [Required]
+    // [Display(Name = "Last Name")]
+    // public string LastName { get; set; }
 
     [Required]
     [Display(Name = "User Name")]
